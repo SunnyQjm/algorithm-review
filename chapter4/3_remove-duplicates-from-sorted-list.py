9ee2fb1ff26b7dbc579faf9bf1c7e0db263513e2
SunnyQjm <qjm253@pku.edu.cn>
2020-06-06 15:16:01 +0800

    add: chapter4

---
+++ chapter4/3_remove-duplicates-from-sorted-list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+################################################################################
+# Leetcode 83 删除排序链表中的重复元素
+#
+# 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+#
+# 示例 1:
+#   输入: 1->1->2
+#   输出: 1->2
+#
+# 示例 2:
+#   输入: 1->1->2->3->3
+#   输出: 1->2->3
+################################################################################
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+    
+    def __repr__(self):
+        if self:
+            return "{}->{}".format(self.val, repr(self.next))
+
+
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtypr ListNode
+
+        (Knowledge)
+
+        1. 用两个指针，pre指向当前遍历节点的前一个节点，cur指向当前节点；
+        2. 用last记录上一次访问的数字；
+        3. 每次查看当前值和上一次访问的数字是否相同，相同则执行删除节点操作，不相同则更新last和两个指针
+        """
+        if not head:
+            return None
+        pre, last, cur = head, head.val, head.next
+        
+        while cur:
+            if cur.val == last:
+                pre.next = cur.next
+            else:
+                last, pre = cur.val, cur
+            cur = cur.next
+
+        return head
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    h1 = ListNode(1)
+    h1.next = ListNode(1)
+    h1.next.next = ListNode(2)
+    print(solution.deleteDuplicates(h1), "= 1->2")
+    
+    h1 = ListNode(1)
+    h1.next = ListNode(1)
+    h1.next.next = ListNode(2)
+    h1.next.next.next = ListNode(3)
+    h1.next.next.next.next = ListNode(3)
+    print(solution.deleteDuplicates(h1), "= 1->2->3")
